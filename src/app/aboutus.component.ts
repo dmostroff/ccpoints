205187a8a87d9f04dee0e0121dec2436e7bc2030
dmostroff <dmostroff@gmail.com>
2017-09-06 09:30:22 +0300

    next version

--- src/app/aboutus.component.ts
+++ src/app/aboutus.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @Component({
   selector: 'app-aboutus',
@@ -6,8 +7,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./aboutus.component.css']
 })
 export class AboutusComponent implements OnInit {
+  name:string;
+  show: boolean = true;
+  showSpinner: boolean = true;
+  size: number;
+  sp: SpinnerComponent;
 
-  constructor() { }
+
+  constructor() {
+    this.name = "Ostroff Enterprises";
+    this.size = 25;
+    this.sp = new SpinnerComponent();
+  }
+  toggleSpinner() {
+    this.show = !this.show;
+    this.showSpinner = this.show;
+    console.log(this.show);
+  }
 
   ngOnInit() {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,40 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MdTableModule} from '@angular/material';
+import { FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  MdAutocompleteModule,
+  MdButtonModule,
+  MdButtonToggleModule,
+  MdCardModule,
+  MdCheckboxModule,
+  MdChipsModule,
+  MdCoreModule,
+  MdDatepickerModule,
+  MdDialogModule,
+  MdExpansionModule,
+  MdGridListModule,
+  MdIconModule,
+  MdInputModule,
+  MdListModule,
+  MdMenuModule,
+  MdNativeDateModule,
+  MdPaginatorModule,
+  MdProgressBarModule,
+  MdProgressSpinnerModule,
+  MdRadioModule,
+  MdRippleModule,
+  MdSelectModule,
+  MdSidenavModule,
+  MdSliderModule,
+  MdSlideToggleModule,
+  MdSnackBarModule,
+  MdSortModule,
+  MdTableModule,
+  MdTabsModule,
+  MdToolbarModule,
+  MdTooltipModule,
+} from '@angular/material';
 import { CdkTable} from '@angular/cdk/table'
 import { CdkTableModule } from '@angular/cdk/table';
 
@@ -16,18 +48,55 @@ import { AppComponent } from './app.component';
 import { AboutusComponent } from './aboutus.component';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SpinnerComponent } from './spinner/spinner.component';
 
 @NgModule({
+  exports: [
+    CdkTableModule,
+    MdAutocompleteModule,
+    MdButtonModule,
+    MdButtonToggleModule,
+    MdCardModule,
+    MdCheckboxModule,
+    MdChipsModule,
+    MdCoreModule,
+    MdDatepickerModule,
+    MdDialogModule,
+    MdExpansionModule,
+    MdGridListModule,
+    MdIconModule,
+    MdInputModule,
+    MdListModule,
+    MdMenuModule,
+    MdNativeDateModule,
+    MdPaginatorModule,
+    MdProgressBarModule,
+    MdProgressSpinnerModule,
+    MdRadioModule,
+    MdRippleModule,
+    MdSelectModule,
+    MdSidenavModule,
+    MdSliderModule,
+    MdSlideToggleModule,
+    MdSnackBarModule,
+    MdSortModule,
+    MdTableModule,
+    MdTabsModule,
+    MdToolbarModule,
+    MdTooltipModule,
+  ],
   declarations: [
     AppComponent,
     AboutusComponent,
     LoginComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    SpinnerComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    MdProgressSpinnerModule,
 
     AppRoutingModule,
     ClientsModule
